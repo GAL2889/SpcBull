f280f136ca04c6a036a89f1dba8370fd94ddac6f
Boris GBAGUIDI <patchocharley@gmail.com>
2016-12-25 00:57:22 +0200

    update the config file

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@ define("DB_SERVER", "localhost");
 define("DB_NAME", "projetscp");
 
 define("DB_TYPE", "mysql");
-define("DB_USER", "root");
-define("DB_PASSWORD", "root");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
 
 //PATH TO ERROR LOG FILE
 define("ERRORLOGFILE_FOLDER", "config/log");
